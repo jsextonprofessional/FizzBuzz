8d1ab7842367f62f159b8cb08dbef978526274f6
jsprofessional <jshuman@protonmail.com>
2021-06-28 17:55:07 -0500

    it works now

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,19 +1,11 @@
-function FizzBuzz(){
-
-    var arr = [];
-        for(var i = 1; i < 101; i++) {
-            if (i % 3 === 0){
-                arr.push("Fizz");
-            }
-            else if (i % 5 === 0){
-                arr.push("Buzz");
-            }
-            else if (i % 3 === 0 && i % 5 ===0){
-                arr.push("FizzBuzz");
-            }
-            else {
-                arr.push(i);
-            }
-        }
-        return arr[];
+for (var i = 1; i < 101; i++) {
+    if (i % 15 == 0) {
+        console.log("FizzBuzz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else {
+        console.log(i);
+    }
 }
