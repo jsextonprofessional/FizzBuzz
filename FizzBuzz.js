ee2c3755f8a5317cdd95387a591dbcde97ea2285
jsprofessional <jshuman@protonmail.com>
2021-06-28 17:44:01 -0500

    first commit

---
+++ FizzBuzz.js
@@ -0,0 +1,19 @@
+function FizzBuzz(){
+
+    var arr = [];
+        for(var i = 1; i < 101; i++) {
+            if (i % 3 === 0){
+                arr.push("Fizz");
+            }
+            else if (i % 5 === 0){
+                arr.push("Buzz");
+            }
+            else if (i % 3 === 0 && i % 5 ===0){
+                arr.push("FizzBuzz");
+            }
+            else {
+                arr.push(i);
+            }
+        }
+        return arr[];
+}
